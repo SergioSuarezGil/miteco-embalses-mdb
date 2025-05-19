a1daa8e8f26c4619d5136f5967404acb89810275
Sergio Suárez <sergiosgil@gmail.com>
2025-05-19 11:13:42 +0200

    feat: added convert-embalses-csv-to.json file

---
+++ convert-embalses-csv-to-json.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
+
+const results = [];
+let lineCount = 0;
+
+fs.createReadStream(path.join(__dirname, 'BD-Embalses.csv'))
+  .pipe(csv({ separator: ',', mapValues: ({ value }) => value.trim() }))
+  .on('data', (data) => {
+    lineCount++;
+
+    data.AGUA_TOTAL = parseFloat(data.AGUA_TOTAL.replace(',', '.'));
+    data.AGUA_ACTUAL = parseFloat(data.AGUA_ACTUAL.replace(',', '.'));
+    data.ELECTRICO_FLAG = parseInt(data.ELECTRICO_FLAG, 10);
+
+    results.push(data);
+  })
+  .on('end', () => {
+    fs.writeFileSync('embalses.json', JSON.stringify(results, null, 2), 'utf-8');
+    console.log(`✅ File: embalses.json sucessfully created`);
+    console.log(`📊 ${lineCount} processed lines`);
+  });
